b5637c14bef501089aa669e581fd53e121d70d15
henny <kimhyein1105@gmail.com>
2023-12-31 22:59:57 +0900

    라우터로 url을 통한 컴포넌트 렌더링 분기처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './styles/Global.scss';
 import Footer from './components/common/footer/Footer';
 import Header from './components/common/header/Header';
-import Department from './components/common/sub/department/Department';
-import Youtube from './components/common/sub/youtube/Youtube';
+import Department from './components/sub/department/Department';
+import Youtube from './components/sub/youtube/Youtube';
+import { Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Department />
-			<Youtube />
+			<Route path='/department'>
+				<Department />
+			</Route>
+
+			<Route path='/youtube'>
+				<Youtube />
+			</Route>
 			<Footer />
 		</>
 	);
