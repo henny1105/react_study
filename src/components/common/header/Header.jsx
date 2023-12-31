b1fbed1f2241e0836c02c262b017360b99c5be5c
henny <kimhyein1105@gmail.com>
2023-12-31 23:21:20 +0900

    Link컴포넌트를 이용해서 메뉴 클릭시 라우터 이동

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,10 +1,20 @@
 import styles from './Header.module.scss';
-import clsx from 'clsx';
-//npm i clsx
-//clsx를 쓰는 이유: 클래스에 변수를 대입할때 일일 템플릿 리터럴처리하기 번거로우므로 함수의 인수전달하듯이 복수개의 클래스 객체를 적용가능
-//삼항연산자를 활용한 분기처리도 가능
+import { Link, NavLink } from 'react-router-dom';
 
-export default function Header(props) {
-	console.log(props);
-	return <header className={clsx(styles.header, props.isMain ? styles.main : null)}>Header</header>;
+export default function Header() {
+	return (
+		<header className={styles.header}>
+			<h1>
+				<Link to='/'>Logo</Link>
+			</h1>
+			<ul>
+				<li>
+					<NavLink to='/department'>Department</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube'>Youtube</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
 }
