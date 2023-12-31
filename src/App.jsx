d39a463fb1ea22cdb1b403425b107e734b84fc57
henny <kimhyein1105@gmail.com>
2023-12-31 23:15:23 +0900

    라우터를 이용해서 메인, 서브 페이지 url로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import Header from './components/common/header/Header';
 import Department from './components/sub/department/Department';
 import Youtube from './components/sub/youtube/Youtube';
 import { Route } from 'react-router-dom';
+import Main from './components/main/Main';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Route exact path='/'>
+				<Main />
+			</Route>
+
 			<Route path='/department'>
 				<Department />
 			</Route>
