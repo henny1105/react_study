82a54d30e013404b1fe97abe180d252fda22d28c
henny <kimhyein1105@gmail.com>
2024-01-02 20:00:14 +0900

    router를 이용해서 url별로 컴포넌트 페이지 분리

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,10 +1,7 @@
-import Layout from '../../common/layout/Layout';
-import styles from './Contact.scss';
-
 export default function Contact() {
 	return (
-		<Layout title={'Contact Us'} className={styles.contact}>
-			<p>찾아오는 길 페이지 전용 컨텐츠가 올자리</p>
-		</Layout>
+		<>
+			<h1>Contact</h1>
+		</>
 	);
 }
--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,10 +1,7 @@
-import Layout from '../../common/layout/Layout';
-import styles from './Department.scss';
-
 export default function Department() {
 	return (
-		<Layout title={'Department'} className={styles.department}>
-			<p>부서소개 페이지 전용 컨텐츠가 올자리</p>
-		</Layout>
+		<>
+			<h1>Department</h1>
+		</>
 	);
 }
