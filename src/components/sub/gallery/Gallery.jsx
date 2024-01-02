bf93dbf142177398cf2cf38750283c0ca5716dd0
henny <kimhyein1105@gmail.com>
2024-01-02 20:37:20 +0900

    layout 컴포넌트에 전달되는 title값을 클래스명에 적합하도록 보정

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,5 +1,10 @@
+import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 
 export default function Gallery() {
-	return <></>;
+	return (
+		<Layout title={'Gallery'}>
+			<p>갤러리 상세페이지</p>
+		</Layout>
+	);
 }
