137b67060e0c10c795f2e6cbaabbdaebdec9b9da
henny <kimhyein1105@gmail.com>
2024-01-07 17:38:53 +0900

    useReftch hook 구조 변경

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,25 +1,26 @@
 import { useFetch } from '../../../hooks/useFetch';
 import Layout from '../../common/layout/Layout';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Youtube() {
 	const [Vids, setVids] = useState([]);
-	//const fetchData = useFetch();
-	console.log(Vids);
+	const fetchData = useFetch();
+	//console.log(Vids);
 
 	useEffect(() => {
 		const api_key = 'AIzaSyD3KmtmTo9yfcsHsZRtsNpjUgUw7rOGdHM';
 		const pid = 'PL5uS0BZeM8zUl73uhFTI6AXaB2C5Y5JJR';
 		const num = 10;
 		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-		//fetchData(baseURL, setVids);
+		fetchData(baseURL, setVids);
 
-		fetch(baseURL)
-			.then((data) => data.json())
-			.then((json) => {
-				console.log(json.items);
-				setVids(json.items);
-			});
+		// fetch(baseURL)
+		// 	.then((data) => data.json())
+		// 	.then((json) => {
+		// 		console.log(json.items);
+		// 		setVids(json.items);
+		// 	});
 	}, []);
 
 	return (
@@ -30,7 +31,9 @@ export default function Youtube() {
 						<h2>{data.snippet.title}</h2>
 						<p>{data.snippet.description}</p>
 						<div className='pic'>
-							<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+							<Link to={`/detail/${data.id}`}>
+								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+							</Link>
 						</div>
 					</article>
 				);
